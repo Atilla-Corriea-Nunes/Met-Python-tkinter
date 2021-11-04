ea342f5fc70b95fa9f0b3980e5deabae538aeb3d
Atilla <you@example.com>
2021-11-04 14:16:04 +0100

    The lightswitch is fully operational!

--- lightswitch.py
+++ lightswitch.py
@@ -1,11 +1,32 @@
 import tkinter as tk
+from tkinter.constants import COMMAND
 window = tk.Tk()
-
-button = tk.Button(text='...', bg="white", fg="black")
-button.pack(pady = 20, padx = 20)
+currentlyOn = False
+currentlyOff = True
 
 # schijf hier tussen je code
 
+def changeSwitchState():
+    global currentlyOff, currentlyOn, button
+
+    if currentlyOff == True:
+        print("light is off")
+        btn_text.set("Light switch off")
+        currentlyOff = False
+        currentlyOn = True
+        window.configure(bg='yellow')
+
+    elif currentlyOn == True:
+        print("light is on")
+        btn_text.set("Light switch on")
+        currentlyOff = True
+        currentlyOn = False
+        window.configure(bg='white')
+
+btn_text = tk.StringVar()
+button = tk.Button(textvariable=btn_text, bg="white", fg="black", command = changeSwitchState)
+button.pack(pady = 20, padx = 20)
+btn_text.set("Light switch on")
 
 
 # schijf hier tussen je code
